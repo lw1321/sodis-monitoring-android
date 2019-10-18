63818592aca2aa9e3f0efab5b24540877b29d535
lw23 <laurin.wandel@googlemail.com>
2019-10-18 23:17:38 +0200

    check if interviewee exists

--- app/src/main/java/de/sodis/monitoring/ui/viewholder/AutoCompleteHeaderViewHolder.kt
+++ app/src/main/java/de/sodis/monitoring/ui/viewholder/AutoCompleteHeaderViewHolder.kt
@@ -11,11 +11,15 @@ import de.sodis.monitoring.ui.model.SodisItem
 import kotlinx.android.synthetic.main.interviewee_item.view.*
 
 
-class AutoCompleteHeaderViewHolder(itemView: View, private val recyclerViewClickListener: RecyclerViewListener) : RecyclerView.ViewHolder(itemView), SodisViewHolder {
+class AutoCompleteHeaderViewHolder(
+    itemView: View,
+    private val recyclerViewClickListener: RecyclerViewListener
+) : RecyclerView.ViewHolder(itemView), SodisViewHolder {
 
     override fun onClick(v: View?) {
         //recyclerViewClickListener.recyclerViewListCLicked(v!!, adapterPosition)
     }
+
     override fun bindView(sodisItem: SodisItem) {
         val item = sodisItem as AutoCompleteHeaderItem
         itemView.title.text = item.title
@@ -28,8 +32,20 @@ class AutoCompleteHeaderViewHolder(itemView: View, private val recyclerViewClick
         itemView.multiAutoCompleteTextView.setAdapter(adapter)
 
         itemView.navigation_forward_button_1.setOnClickListener {
-            if (itemView.multiAutoCompleteTextView.text.isEmpty()){
-                Toast.makeText(itemView.context,"Ingrese el nombre del encuestado", Toast.LENGTH_LONG).show()
+            if (itemView.multiAutoCompleteTextView.text.isEmpty()) {
+                Toast.makeText(
+                    itemView.context,
+                    "Ingrese el nombre del encuestado",
+                    Toast.LENGTH_LONG
+                ).show()
+                return@setOnClickListener
+            }
+            if (!item.list.contains(itemView.multiAutoCompleteTextView.text.toString())) {
+                Toast.makeText(
+                    itemView.context,
+                    "Nombre no encontrado",
+                    Toast.LENGTH_LONG
+                ).show()
                 return@setOnClickListener
             }
             recyclerViewClickListener.recyclerViewListCLicked(itemView, adapterPosition)
