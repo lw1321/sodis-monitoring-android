4709d6a086bab9c2b5400338dd4fa3b36d45afc1
lw23 <laurin.wandel@googlemail.com>
2020-03-06 22:54:03 +0100

    Update endpoint url

--- app/src/main/java/de/sodis/monitoring/Config.kt
+++ app/src/main/java/de/sodis/monitoring/Config.kt
@@ -2,6 +2,7 @@ package de.sodis.monitoring
 
 class Config {
     companion object {
-        const val MONITORING_API_DEV = "https://sodis-survey-api.herokuapp.com/v1/"
+        const val MONITORING_API_DEV = "https://sodis-survey-api-dev.herokuapp.com/v1/"
+        const val MONITORING_API_TEST = "https://sodis-survey-api-test.herokuapp.com/v1/"
     }
 }
