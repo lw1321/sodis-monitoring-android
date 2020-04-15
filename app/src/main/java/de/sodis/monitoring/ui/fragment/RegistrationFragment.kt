e756088b77c94b51fc0dc9088ad2c9bdc8e8f4f5
lw23 <laurin.wandel@googlemail.com>
2020-04-15 13:04:26 +0200

    missing import

--- app/src/main/java/de/sodis/monitoring/ui/fragment/RegistrationFragment.kt
+++ app/src/main/java/de/sodis/monitoring/ui/fragment/RegistrationFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isGone
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
