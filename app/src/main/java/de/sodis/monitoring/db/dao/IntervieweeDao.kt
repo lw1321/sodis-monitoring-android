b31d2f87b09de74bec268cd64b1a0e2525064698
laurin <laurin.wandel@googlemail.com>
2021-04-05 00:11:41 +0200

    order families by name

--- app/src/main/java/de/sodis/monitoring/db/dao/IntervieweeDao.kt
+++ app/src/main/java/de/sodis/monitoring/db/dao/IntervieweeDao.kt
@@ -37,7 +37,7 @@ interface IntervieweeDao {
     @Query("SELECT * FROM Interviewee WHERE imagePath IS NOT NULL AND imageUrl IS NULL ")
     fun getNotsyncedProfilePictures(): List<Interviewee>
 
-    @Query("SELECT i.id, i.name, i.imagePath, v.name as villageName, v.id as villageId FROM Interviewee i JOIN village v ON v.id = i.villageId")
+    @Query("SELECT i.id, i.name, i.imagePath, v.name as villageName, v.id as villageId FROM Interviewee i JOIN village v ON v.id = i.villageId ORDER BY i.name")
     fun getFamilyList(): LiveData<List<IntervieweeItem>>
 
     @Query("SELECT i.name FROM Interviewee i WHERE id=:family")
